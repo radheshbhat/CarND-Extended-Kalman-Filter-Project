d40745ce80794925e643d1f2e4f137f15a7450d0
Radhesh Bhat <radhesh.bhat@pathpartnertech.com>
2019-02-09 12:44:00 +0530

    EKF implementation

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -1,42 +1,46 @@
 #ifndef FusionEKF_H_
 #define FusionEKF_H_
 
-#include <fstream>
-#include <string>
-#include <vector>
-#include "Eigen/Dense"
-#include "kalman_filter.h"
 #include "measurement_package.h"
+#include "Eigen/Dense"
+#include <vector>
+#include <string>
+#include <fstream>
+#include "kalman_filter.h"
 #include "tools.h"
 
 class FusionEKF {
- public:
+public:
   /**
-   * Constructor.
-   */
+  * Constructor.
+  */
   FusionEKF();
 
   /**
-   * Destructor.
-   */
+  * Destructor.
+  */
   virtual ~FusionEKF();
 
   /**
-   * Run the whole flow of the Kalman Filter from here.
-   */
+  * Run the whole flow of the Kalman Filter from here.
+  */
   void ProcessMeasurement(const MeasurementPackage &measurement_pack);
 
   /**
-   * Kalman Filter update and prediction math lives in here.
-   */
+  * Kalman Filter update and prediction math lives in here.
+  */
   KalmanFilter ekf_;
 
- private:
+private:
   // check whether the tracking toolbox was initialized or not (first measurement)
   bool is_initialized_;
 
   // previous timestamp
   long long previous_timestamp_;
+
+  // Noise values from the task
+  const float noise_ax = 9.0;
+  const float noise_ay = 9.0;
 
   // tool object used to compute Jacobian and RMSE
   Tools tools;
@@ -46,4 +50,4 @@ class FusionEKF {
   Eigen::MatrixXd Hj_;
 };
 
-#endif // FusionEKF_H_
+#endif /* FusionEKF_H_ */
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,10 +3,8 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
- *   VectorXd or MatrixXd objects with zeros upon creation.
- */
+// Please note that the Eigen library does not initialize 
+// VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
 
@@ -23,19 +21,47 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+	double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+	if (fabs(rho) > 0.0001 && (fabs(x_(0)) > 0.0001 && fabs(x_(1)) > 0.0001)) {
+		double theta = atan2(x_(1),x_(0));
+	    double rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
+
+	    VectorXd h = VectorXd(3);
+	    h << rho, theta, rho_dot;
+		VectorXd y = z - h;
+		y(1) = atan2( sin(y(1)), cos(y(1)) ); // Angle normalization between pi and -pi
+		MatrixXd Ht = H_.transpose();
+		MatrixXd PHt = P_ * Ht;
+		MatrixXd S = H_ * PHt + R_;
+		MatrixXd Si = S.inverse();
+		MatrixXd K = PHt * Si;
+
+		//new estimate
+		x_ = x_ + (K * y);
+		long x_size = x_.size();
+		MatrixXd I = MatrixXd::Identity(x_size, x_size);
+		P_ = (I - K * H_) * P_;
+	}
 }
